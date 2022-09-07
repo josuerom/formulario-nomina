eafd8a1fab561db0471b6a0a86b49cad5309d10f
Josue Romero <josue.romero@uniminuto.edu.co>
2022-09-07 00:51:00 -0500

    Segundo ajuste

--- php/main.php
+++ php/main.php
@@ -1,5 +1,73 @@
 <?php
    if (isset($_POST['enviar'])) {
-      
+      if (!empty($_POST['nombres']) && !empty($_POST['salario']) && !empty($_POST['dias_laborados']) && !empty($_POST['comisiones']) && !empty($_POST['libranza'])) {
+         $nombres = $_POST['nombres'];
+         $salario_mensual = (int) $_POST['salario'];
+         $dias_laborados = (int) $_POST['dias_laborados'];
+         $hdiurnas = (int) $_POST['horas_diurnas'];
+         $hnocturnas = (int) $_POST['horas_nocturnas'];
+         $hdominicales = (int) $_POST['horas_dominicales'];
+         $comisiones = (int) $_POST['comisiones'];
+         $libranza = (int) $_POST['libranza'];
+         
+         $total_salario = (salario_mensual / 30);
+         $total_salario = ($total_salario * $dias_laborados);
+         
+         $HD = 1500;
+         $HN = 2000;
+         $HDOM = 2500;
+         $VHD = ($hdiurnas * $HD);
+         $VHN = ($hnocturnas * $HN);
+         $VHDOM = ($hdominicales * $HDOM);
+         
+         $valor_total = ($VHD + $VHN + $VHDOM);
+         $total_devengado = ($salario_mensual + $valor_total);
+
+         $arregloNomina = [
+            [
+                    'Nombre y apellido',
+                    'Salario mensual',
+                    'Días laborados',
+                    'Total salario',
+                    'Horas Diurnas',
+                    'Horas Nocturnas',
+                    'Horas Dominicales',
+                    'VHD',
+                    'VHN',
+                    'VHDOM',
+                    'Valor Total',
+                    'Comisiones',
+                    'Total Devengado',
+                    'Libranza'
+            ],
+            [
+                    $nombres,
+                    $salario,
+                    $dias_laborados,
+                    $total_salario,
+                    $hdiurnas,
+                    $hnocturnas,
+                    $hdominicales,
+                    $VHD,
+                    $VHN,
+                    $VHDOM,
+                    $valor_total,
+                    $comisiones,
+                    $total_devengado,
+                    $libranza,
+            ]
+         ];
+
+         $s = '<table border="1">';
+         foreach ( $arregloNomina as $r ) {
+            $s .= '<tr>';
+            foreach ( $r as $v ) {
+               $s .= '<td>'.$v.'</td>';
+            }
+            $s .= '</tr>';
+         }
+         $s .= '</table>';
+         echo $s;
+      }
    }
 ?>
